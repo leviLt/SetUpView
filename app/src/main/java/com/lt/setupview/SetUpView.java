70dbf9b8b0c946b75e515e12d5bfc9e55f4182d6
luotao <751423471@qq.com>
2018-09-13 10:21:14 +0800

    添加常量

--- app/src/main/java/com/lt/setupview/SetUpView.java
+++ app/src/main/java/com/lt/setupview/SetUpView.java
@@ -6,10 +6,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Scorpio
@@ -18,6 +23,14 @@ import android.view.View;
  * phone:13982250340
  */
 public class SetUpView extends View {
+
+    private static final int DEFAULT_HEIGHT = 85; //默认高度
+    private static final int DEFAULT_PADDING = 10; //默认padding值
+    private static final int DAYS_MARGIN_TOP = 13; // 文字距离的marginTop值
+    private static final float SETUP_HEXAGON_SCALE = 1F / 6; //六边形的缩放值
+    private static final float SETUP_LINE_BG_SCALE = 1F / 4; //横线的 缩放值
+    private static final float SECTION_SCALE = 1.2F / 2; //截面的缩放值
+
     private Context mContext;
     /*--------------------颜色------------------------*/
     //签到背景颜色
@@ -30,7 +43,7 @@ public class SetUpView extends View {
     private int mSetUpDaysColor;
     //下方日期的大小
     private int mSetUpDaysSize;
-    /*----------------------------------------------*/
+    /*-----------------------颜色-----------------------*/
     /*----------------------画笔-----------------------*/
     //签到背景画笔
     private Paint mSetUpBgPaint;
@@ -45,6 +58,22 @@ public class SetUpView extends View {
     private Bitmap giftBitmap;
     //礼物矩形区域
     private RectF giftRecf;
+    //padding
+    private int mPadding;
+    //控件宽
+    private int mSetUpViewWidth;
+    //控件高度
+    private int mSetUpViewHeight;
+    //六边形的半径
+    private int mHexagonRadius;
+    //横线的高度
+    private int mLineHeight;
+    //控件宽度直线
+    private RectF mSetUpLineRecf;
+    //签到时间
+    private List<String> days;
+    //六边形的Y轴坐标
+    private int circleY;
 
     public SetUpView(Context context) {
         this(context, null);
@@ -73,6 +102,13 @@ public class SetUpView extends View {
         giftBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.img_signpage_gift);
         //礼物矩形区域
         giftRecf=new RectF(0,0,giftBitmap.getWidth(),giftBitmap.getHeight());
+
+        days=new ArrayList<>();
+        days.add("1");
+        days.add("2");
+        days.add("3");
+        days.add("4");
+        days.add("5");
     }
 
 
@@ -123,5 +159,41 @@ public class SetUpView extends View {
          */
         paint.setStrokeJoin(Paint.Join.ROUND);
         return paint;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        //转换padding px2dp
+        mPadding= (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,DEFAULT_PADDING,getResources().getDisplayMetrics());
+        //文字距离上方的距离 px2dp
+       int  daysMarginTop= (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, DAYS_MARGIN_TOP,getResources().getDisplayMetrics());
+       //控件的宽高
+        mSetUpViewHeight=h;
+        mSetUpViewWidth=w;
+        //六边形的半径
+        mHexagonRadius= (int) (mSetUpViewHeight*SETUP_HEXAGON_SCALE/2);
+        //横线的高度
+        mLineHeight= (int) (mHexagonRadius*SETUP_LINE_BG_SCALE);
+
+        mSetUpLineRecf=new RectF(0,mSetUpViewHeight*SECTION_SCALE-mHexagonRadius-mLineHeight
+                ,mSetUpViewWidth,mSetUpViewHeight*SECTION_SCALE-mHexagonRadius);
+
+        circleY= (int) (mSetUpLineRecf.top+mLineHeight/2);
+        //计算点和图形的位置
+        calculateCirclePoints();
+    }
+
+    private void calculateCirclePoints() {
+        if (days!=null){
+            int viewCount=days.size()+1;
+            //控件宽度中，计算每段距离大小
+            int oncePiece=(mSetUpViewWidth-mHexagonRadius*2*viewCount)/viewCount;
+
+            for (int i = 0; i < days.size(); i++) {
+                //每个六边形的 中心点位置
+                Point circlePoint=new Point((i+1)*oncePiece+((i+1)*2-1)*oncePiece,circleY);
+            }
+        }
     }
 }
